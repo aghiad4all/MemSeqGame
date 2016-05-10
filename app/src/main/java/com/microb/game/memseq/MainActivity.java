4e372a328d2e32069a21192863e5550392b2530d
ablack <aghiad4all@hotmail.com>
2016-05-10 11:46:59 +0300

    Added activity with sound button

--- app/src/main/java/com/microb/game/memseq/MainActivity.java
+++ app/src/main/java/com/microb/game/memseq/MainActivity.java
@@ -1,13 +1,34 @@
 package com.microb.game.memseq;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StartButtonListener();
+    }
+    public void StartButtonListener(){
+        start=(ImageButton)findViewById(R.id.imageButton);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.start);
+        start.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent aintent = new Intent(MainActivity.this, game_start.class);
+                mp.start();
+                startActivity(aintent);
+            }
+
+
+        });
     }
 }
