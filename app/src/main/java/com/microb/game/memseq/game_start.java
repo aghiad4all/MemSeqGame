f52a22ee5237a14a72ae575197db16112460b285
Aghiad Black <ablack@accumed>
2016-05-10 09:17:17 +0300

    Adding files in project 'MemSeq' to Git...

---
+++ app/src/main/java/com/microb/game/memseq/game_start.java
@@ -0,0 +1,13 @@
+package com.microb.game.memseq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class game_start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_start);
+    }
+}
